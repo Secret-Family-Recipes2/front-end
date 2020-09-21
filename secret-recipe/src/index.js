0a2c163ab64154c3073d29c8b4bc6672301e1ff1
Emmanuel Padilla <manolin923@gmail.com>
2020-09-21 11:15:57 -0700

    installed dependancies on test branch

--- secret-recipe/src/index.js
+++ secret-recipe/src/index.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
+  // Wrap the <App /> in a provider
+  <Router>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </Router>,
+  document.querySelector("#root")
 );
 
 // If you want your app to work offline and load faster, you can change
